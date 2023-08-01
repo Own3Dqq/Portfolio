e64b779050eca8b031b37ca6efca9bbcd2025acd
Own3Dqq <own3dqq@gmail.com>
2023-08-01 20:16:12 +0300

    init project

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+import Avatar from '../components/Avatar';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+
+const Home = () => {
+    return (
+        <div className='bg-primary/60 h-full'>
+            <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+                <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+                    {/* title */}
+                    <h1 className='h1'>
+                        Transforming Ideas <br /> Into <span className='text-accent'>Digital Reality</span>
+                    </h1>
+                    {/*  */}
+                    <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos repellat quas qui hic temporibus
+                        voluptates nemo magni, facere vitae laborum ducimus deserunt et? Omnis veniam eligendi ipsa odio
+                        nemo eius.
+                    </p>
+                </div>
+            </div>
+            {/* image */}
+            <div>image</div>
+        </div>
+    );
+};
+
+export default Home;
