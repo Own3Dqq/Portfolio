cf27e655daaccb5d997b11414cff2b65a714af84
Own3Dqq <own3dqq@gmail.com>
2023-08-02 21:39:22 +0300

    fix link

--- components/Socials.js
+++ components/Socials.js
@@ -4,18 +4,30 @@ import { RiLinkedinFill, RiTelegramFill, RiFacebookFill, RiGithubFill } from 're
 const Socials = () => {
     return (
         <div className='flex justify-between items-center gap-x-4 text-lg'>
-            <Link href={'www.linkedin.com/in/own3dqq'} className='hover:text-accent transition-all duration-300'>
+            <a
+                href='https://linkedin.com/in/own3dqq'
+                target='_blank'
+                className='hover:text-accent transition-all duration-300'
+            >
                 <RiLinkedinFill size={22} />
-            </Link>
-            <Link href={'https://github.com/Own3Dqq'} className='hover:text-accent transition-all duration-300'>
+            </a>
+            <a
+                href='https://github.com/Own3Dqq'
+                target='_blank'
+                className='hover:text-accent transition-all duration-300'
+            >
                 <RiGithubFill size={22} />
-            </Link>
-            <Link href={'https://www.facebook.com/Tsarik.V/'} className='hover:text-accent transition-all duration-300'>
+            </a>
+            <a
+                href='https://www.facebook.com/Tsarik.V/'
+                target='_blank'
+                className='hover:text-accent transition-all duration-300'
+            >
                 <RiFacebookFill size={22} />
-            </Link>
-            <Link href={'https://t.me/Own3Dqq'} className='hover:text-accent transition-all duration-300'>
+            </a>
+            <a href='https://t.me/Own3Dqq' target='_blank' className='hover:text-accent transition-all duration-300'>
                 <RiTelegramFill size={22} />
-            </Link>
+            </a>
         </div>
     );
 };
