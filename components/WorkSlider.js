51b431af0d8f9b867521478385f5e528813fc4fe
Own3Dqq <own3dqq@gmail.com>
2023-08-02 15:55:23 +0300

    implemented contact page

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,51 +1,105 @@
 // data
 const workSlides = {
-  slides: [
-    {
-      images: [
+    slides: [
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+            images: [
+                {
+                    title: 'Landing Page - Ben Aiden Professional Web Design',
+                    path: '/thumb1.jpg',
+                    url: '/',
+                },
+                {
+                    title: 'Landing Page - UI/UX Design Master',
+                    path: '/thumb2.jpg',
+                    url: '/',
+                },
+                {
+                    title: 'MultiPage Site - Crypto Invest Platform',
+                    path: '/thumb3.jpg',
+                    url: '/',
+                },
+                {
+                    title: 'MultiPage Site - Music Accessories',
+                    path: '/thumb4.jpg',
+                    url: '/',
+                },
+            ],
         },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-      ],
-    },
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-      ],
-    },
-  ],
+        // {
+        //     images: [
+        //         {
+        //             title: 'title',
+        //             path: '/thumb4.jpg',
+        //             url: '/',
+        //         },
+        //         {
+        //             title: 'title',
+        //             path: '/thumb1.jpg',
+        //             url: '/',
+        //         },
+        //         {
+        //             title: 'title',
+        //             path: '/thumb2.jpg',
+        //             url: '/',
+        //         },
+        //         {
+        //             title: 'title',
+        //             path: '/thumb3.jpg',
+        //             url: '/',
+        //         },
+        //     ],
+        // },
+    ],
 };
 
+import Link from 'next/link';
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import {} from 'react-icons/bs';
+
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+    return (
+        <Swiper
+            spaceBetween={10}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination]}
+            className='h-[280px] sm:h-[480px]'
+        >
+            {workSlides.slides.map((slide, sliderIndex) => {
+                return (
+                    <SwiperSlide key={sliderIndex}>
+                        <div className='grid grid-cols-2 grid-rows-2 gap-4'>
+                            {slide.images.map((image, index) => {
+                                return (
+                                    <Link key={index} href={image.url}>
+                                        <div className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
+                                            <div className='flex items-center justify-center relative overflow-hidden cursor-pointer'>
+                                                <Image src={image.path} width={500} height={300} alt='' className='' />
+                                                {/* overlay */}
+                                                <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'>
+                                                    <div className='flex justify-center items-center w-full h-full p-12 text-center'>
+                                                        {image.title}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                );
+                            })}
+                        </div>
+                    </SwiperSlide>
+                );
+            })}
+        </Swiper>
+    );
 };
 
 export default WorkSlider;
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,14 @@
 import { FaHtml5, FaCss3, FaJs, FaReact, FaWordpress, FaFigma } from 'react-icons/fa';
-import { SiNextdotjs, SiFramer, SiAdobexd, SiAdobephotoshop } from 'react-icons/si';
+import {
+    SiNextdotjs,
+    SiFramer,
+    SiAdobexd,
+    SiAdobephotoshop,
+    SiTailwindcss,
+    SiRedux,
+    SiMui,
+    SiBootstrap,
+} from 'react-icons/si';
 import React, { useState } from 'react';
 import { fadeIn } from '../../variants';
 //  data
@@ -9,58 +18,58 @@ const aboutData = [
         info: [
             {
                 title: 'Web Development',
-                icons: [<FaHtml5 />, <FaCss3 />, <FaJs />, <FaReact />, <SiNextdotjs />, <SiFramer />, <FaWordpress />],
+                icons: [<FaHtml5 />, <FaCss3 />, <FaJs />, <FaReact />, , <SiRedux />, <SiNextdotjs />],
             },
             {
                 title: 'UI/UX Design',
-                icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+                icons: [<FaFigma />, <SiAdobephotoshop />],
+            },
+            {
+                title: 'Other Tools',
+                icons: [<SiTailwindcss />, <SiMui />, <SiBootstrap />],
             },
         ],
     },
     {
         title: 'awards',
         info: [
-            {
-                title: 'Webby Awards - Honoree',
-                stage: '2011 - 2012',
-            },
-            {
-                title: 'Adobe Design Achievement Awards - Finalist',
-                stage: '2009 - 2010',
-            },
+            // {
+            //     title: 'Webby Awards - Honoree',
+            //     stage: '2011 - 2012',
+            // },
+            // {
+            //     title: 'Adobe Design Achievement Awards - Finalist',
+            //     stage: '2009 - 2010',
+            // },
         ],
     },
     {
         title: 'experience',
         info: [
             {
-                title: 'UX/UI Designer - XYZ Company',
-                stage: '2012 - 2023',
+                title: 'Front-End Developer - UpWork',
+                stage: '2023',
             },
-            {
-                title: 'Web Developer - ABC Agency',
-                stage: '2010 - 2012',
-            },
-            {
-                title: 'Intern - DEF Corporation',
-                stage: '2008 - 2010',
-            },
+            // {
+            //     title: 'Web Developer - ABC Agency',
+            //     stage: '2010 - 2012',
+            // },
+            // {
+            //     title: 'Intern - DEF Corporation',
+            //     stage: '2008 - 2010',
+            // },
         ],
     },
     {
         title: 'credentials',
         info: [
             {
-                title: 'Web Development - ABC University, LA, CA',
-                stage: '2011',
+                title: 'Certified Web Development - wayup.com Online Course',
+                stage: '2019',
             },
             {
-                title: 'Computer Science Diploma - AV Technical Institute',
-                stage: '2009',
-            },
-            {
-                title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-                stage: '2006',
+                title: 'Computer Engineering Diploma - UA Technical University',
+                stage: '2019',
             },
         ],
     },
@@ -70,6 +79,8 @@ import Avatar from '../../components/Avatar';
 import Circles from '../../components/Circles';
 import { motion } from 'framer-motion';
 
+import CountUp from 'react-countup';
+
 const About = () => {
     const [index, setIndex] = useState(0);
 
@@ -77,11 +88,32 @@ const About = () => {
         <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
             <Circles />
             {/* avatar */}
-            <motion.div variants={fadeIn('right', 0.2)} className='hidden xl:flex absolute bottom-0 -left-[370px]'>
+            {/* <motion.div variants={fadeIn('right', 0.2)} className='hidden xl:flex absolute bottom-0 -left-[370px]'>
                 <Avatar />
-            </motion.div>
+            </motion.div> */}
             <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
-                <div className='flex-1 flex flex-col justify-center'>text</div>
+                <div className='flex-1 flex flex-col justify-center'>
+                    <motion.h2
+                        variants={fadeIn('up', 0.5)}
+                        initial='hidden'
+                        animate='show'
+                        exit='hidden'
+                        className='h2'
+                    >
+                        Captivating <span className='text-accent'>stories</span> birth magificent designs.
+                    </motion.h2>
+                    <motion.p
+                        variants={fadeIn('up', 0.5)}
+                        initial='hidden'
+                        animate='show'
+                        exit='hidden'
+                        className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'
+                    >
+                        1 years ago, I began freelancing as a developer. Since then, I've done remove work for agencies,
+                        consulted for startups, and collaborated on digital products for business and consumer use.
+                    </motion.p>
+                </div>
+                {/* info */}
                 <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
                     <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
                         {aboutData.map((item, itemIndex) => {
@@ -118,16 +150,19 @@ const About = () => {
                                     className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items:center text-white/60'
                                 >
                                     {/* title */}
-                                    <div>{item.title}</div>
-                                    <div className='hidden md:flex '></div>
+                                    <div className='font-light mb-2 md:md-0'>{item.title}</div>
+                                    <div className='hidden md:flex'>-</div>
                                     <div>{item.stage}</div>
-                                    {item.icons?.map((icon, index) => {
-                                        return (
-                                            <div key={index} className='text-2xl text-white'>
-                                                {icon}
-                                            </div>
-                                        );
-                                    })}
+                                    <div className='flex gap-x-4'>
+                                        {/* icons */}
+                                        {item.icons?.map((icon, index) => {
+                                            return (
+                                                <div key={index} className='text-2xl flex text-white'>
+                                                    {icon}
+                                                </div>
+                                            );
+                                        })}
+                                    </div>
                                 </div>
                             );
                         })}
